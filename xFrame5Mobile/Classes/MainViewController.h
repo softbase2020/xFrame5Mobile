54a509c88a42097ca64adfb1be99e93e543ecb1e
hansne <hans@directionsoft.com>
2021-02-19 02:17:58 +0900

    MainViewController 파일 추가

---
+++ xFrame5Mobile/Classes/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  xFrame5Mobile
+//
+//  Created by Sanghong Han on 2021/02/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ xFrame5Mobile/Classes/MainViewController.m
@@ -0,0 +1,21 @@
+//
+//  MainViewController.m
+//  xFrame5Mobile
+//
+//  Created by Sanghong Han on 2021/02/19.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
